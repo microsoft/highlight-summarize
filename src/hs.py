07035158c28c30beda818101b108d87b23cd0ad0
Giovanni Cherubin <gcherubin@microsoft.com>
2025-06-02 08:56:53 +0100

    refactor: hs uses types

--- src/hs.py
+++ src/hs.py
@@ -3,11 +3,25 @@ from textwrap import dedent
 from types import FunctionType
 from pydantic import BaseModel
 
-from qa import QAEvaluator
+from .qa import QAEvaluator, QAPrediction
+from .utils import NOANSWER_PRED, FAILED_PRED
+
+class HighlighterOutput(BaseModel):
+    highlighter_extracted: str | None = None
+    highlighter_llm_response: str | None = None
+    highlighter_text_extracts: list[str] | None = None
+    highlighter_fuzzmatch_scores: list[float] | None = None
 
 class SummarizerOutput(BaseModel):
-    guessed_question: str
-    answer: str
+    anser_pred: str | None = None
+    summarizer_llm_response: str | None = None
+    summarizer_llm_guessed_question: str | None = None
+
+class HSBaselinePrediction(QAPrediction, HighlighterOutput, SummarizerOutput):
+    """Prediction made by the H&S pipeline.
+    """
+    pass
+
 
 class HSBaseline(QAEvaluator):
     """H&S based on two LLMs.
@@ -31,16 +45,16 @@ class HSBaseline(QAEvaluator):
         )
         
         self.extractor_prompt = dedent(
-            """You are an expert research assistant.
-            You are tasked with highlighting relevant text extract(s) from a context text.
-            Your output must be (verbatim) a list of the text extract(s) that answer the question.
-            You will output the text extract(s), by prefixing each extract with a bullet point "-",
-            and nothing else.
-            Important: if no part of the text answers the question, you must output "UNANSWERABLE".
-            Context:
-            {context}
-            Question: {question_str}?\n
-            """)
+            "You are an expert research assistant."
+            "You are tasked with highlighting relevant text extract(s) from a context text."
+            "Your output must be (verbatim) a list of the text extract(s) that answer the question."
+            "You will output the text extract(s), by prefixing each extract with a bullet point '-',"
+            "and nothing else."
+            f"Important: if no part of the text answers the question, you must output '{NOANSWER_PRED}'.\n"
+            "Context:\n"
+            "{context}\n"
+            "Question: {question_str}?\n"
+            )
         self.summarizer_prompt = dedent(
             """You are given highlighted text from a document; the text extract is relevant to some
             question which you don't know. Figure out what question the text extract is trying
@@ -49,22 +63,26 @@ class HSBaseline(QAEvaluator):
             {text_extract}
             """)
 
-    def call_model(self, context_str: str, question_str: str) -> tuple[str | None, str | None]:
-        # NOTE: we can't be too liberal with the metadata fields here: we need
-        # to specify them in advance, or the HF Dataset will fail to create.
-        metadata_fields = ["raw_response", "guessed_question", "raw_text_extracts"]
-        metadata = {field: None for field in metadata_fields}
-        highlighted, highliter_meta = self.call_highlighter(context_str, question_str)
-        metadata.update(highliter_meta)
+    def call_model(self, context_str: str, question_str: str) -> HSBaselinePrediction:
+        highlighted = self.call_highlighter(context_str, question_str)
 
-        if not highlighted:
-            return "UNANSWERABLE", metadata
+        if not highlighted.highlighter_extracted:
+            return HSBaselinePrediction(
+                # Refuse to answer (no highlight).
+                answer_pred=NOANSWER_PRED,
+                **highlighted.model_dump(),
+            )
 
-        answer, summarizer_meta = self.call_summarizer(highlighted)
-        metadata.update(summarizer_meta)
-        return answer, metadata
+        summarized = self.call_summarizer(highlighted)
+
+        return HSBaselinePrediction(
+            **highlighted.model_dump(),
+            **summarized.model_dump(),
+            model_name=self.model_name,
+            temperature=self.temperature,
+        )
         
-    def call_highlighter(self, context_str: str, question_str: str) -> tuple[str | None, dict[str, str]]:
+    def call_highlighter(self, context_str: str, question_str: str) -> HighlighterOutput:
         model_response = self.openai_client().chat.completions.create(
             messages=[
                 {"role": "user", "content": self.extractor_prompt.format(
@@ -75,39 +93,45 @@ class HSBaseline(QAEvaluator):
             temperature=self.temperature,
             model=self.model_name,
         )
-        # Parse.
+
+        # No response.
         if not model_response.choices or not model_response.choices[0].message.content:
-            return None, {}
-        if not model_response.choices[0].message.content.startswith("- "):
-            # print(f"Unexpected response format: {model_response.choices[0].message.content}")
-            return None, {"raw_response": model_response.choices[0].message.content}
+            return HighlighterOutput()
+        content = model_response.choices[0].message.content
+        # Malformed output.
+        if not content.strip().startswith("- "):
+            return HighlighterOutput(highlighter_llm_response=content)
+        # Nothing to highlight.
+        if NOANSWER_PRED in text_extracts:
+            return HighlighterOutput(highlighter_llm_response=content)
         # Extract the text extract(s) from the response.
-        text_extracts = model_response.choices[0].message.content.strip().split("\n")
-        if "UNANSWERABLE" in text_extracts:
-            return None, {"raw_response": model_response.choices[0].message.content}
-        # Remove the bullet points and strip whitespace.
-        text_extracts = [extract.strip().lstrip("- ") for extract in text_extracts if extract.strip()]
+        text_extracts = content.strip().split("\n")
+        text_extracts = [extract.strip().lstrip("- ") for extract in text_extracts]
         if not text_extracts:
-            # print("No text extracts found in the response.")
-            return None, {"raw_response": model_response.choices[0].message.content}
+            return HighlighterOutput(highlighter_llm_response=content)
         
         # Check if the text extracts are in the context.
         valid_text_extracts = []
+        scores = []
         for text_extract in text_extracts:
-            score = fuzz.partial_ratio(text_extract, context_str)
-            if score < 95:
-                    print(f"Score: {score}")
-                    print(f"Text extract not found in context: {text_extract}")
-                    print(f"Context: {context_str}")
-            else:
+            scores.append(fuzz.partial_ratio(text_extract, context_str))
+            if scores[-1] >= 95:
                 valid_text_extracts.append(text_extract)
         
         valid_text = "\n".join(valid_text_extracts)
 
-        return valid_text.strip() if valid_text else None, {"raw_text_extracts": text_extracts}
+        return HighlighterOutput(
+            highlighter_extracted=valid_text.strip(),
+            highlighter_llm_response=content,
+            highlighter_text_extracts=text_extracts,
+            highlighter_fuzzmatch_scores=scores,
+        )
 
-    def call_summarizer(self, text_extract: str) -> str:
+    def call_summarizer(self, text_extract: str) -> SummarizerOutput:
         # Structured output: https://learn.microsoft.com/en-us/azure/ai-services/openai/how-to/structured-outputs?tabs=python-secure%2Cdotnet-entra-id&pivots=programming-language-python.
+        class LLMSummarizerOutput(BaseModel):
+            guessed_question: str
+            answer: str
         raw_response = self.openai_client().beta.chat.completions.parse(
             messages=[
                 {"role": "user", "content": self.summarizer_prompt.format(
@@ -116,14 +140,12 @@ class HSBaseline(QAEvaluator):
             ],
             temperature=self.temperature,
             model=self.model_name,
-            response_format=SummarizerOutput,
+            response_format=LLMSummarizerOutput,
         ).choices[0].message.parsed
 
-        if not raw_response.answer:
-            print(f"Empty answer from summarizer: {raw_response}")
-            return "UNANSWERABLE", {"raw_response": raw_response}
-        if not hasattr(raw_response, "guessed_question") or not raw_response.guessed_question:
-            print(f"Empty guessed question from summarizer: {raw_response}")
-            return raw_response.answer, {"raw_response": raw_response}
-        
-        return raw_response.answer, {"guessed_question": raw_response.guessed_question}
+        return SummarizerOutput(
+            # Failed prediction if the LLM gives no answer.
+            answer_pred=raw_response.answer if hasattr(raw_response, "answer") else FAILED_PRED,
+            summarizer_llm_response=raw_response,
+            summarizer_llm_guessed_question=raw_response.guessed_question if hasattr(raw_response, "guessed_question") else None,
+        )
