d4477fc2a286b3c0ad7626ced1285a19602ed8c5
Giovanni Cherubin <gcherubin@microsoft.com>
2025-06-09 11:02:16 +0000

    implement run_experiments script + config

---
+++ run_experiments.py
@@ -0,0 +1,184 @@
+import os
+import yaml
+import time
+import datasets
+from typing import Any
+
+from src.qa import QAEvaluator
+from src.data import load_dataset
+from src.utils import openai_client
+from src.judges import LLMJudgeStructured
+from src.hs import HSBaseline, HSStructuredHighlighter, HSBERTExtractor
+
+
+N_PROC = 1
+N_TRIALS = 5
+
+PIPELINE_MAP = {
+    "QAEvaluator": QAEvaluator,
+    "HSBaseline": HSBaseline,
+    "HSStructuredHighlighter": HSStructuredHighlighter,
+    "HSBERTExtractor": HSBERTExtractor,
+}
+
+def run_id(experiment_config, dataset_name):
+    """Generate a unique run ID for the experiment based on its configuration and dataset name.
+    The run ID is also the directory where the results will be stored.
+    """
+    if experiment_config["pipeline"] == "QAEvaluator":
+        return (
+            f"{experiment_config['results_dir']}/"
+            f"{dataset_name}/{experiment_config['pipeline']}-{experiment_config['model_name']}"
+        )
+
+    return (
+        f"{experiment_config['results_dir']}/"
+        f"{dataset_name}/{experiment_config['pipeline']}"
+        f"-{experiment_config['highlighter_model_name']}-{experiment_config['summarizer_model_name']}"
+    )
+
+def load_config(path):
+    """Load the experiment configuration from a YAML file.
+    """
+    with open(path, "r") as f:
+        config = yaml.safe_load(f)
+    # Experiments are repeated for each dataset.
+    experiments = {}
+    for dataset_name in config["datasets"]:
+        for experiment_config in config["experiments"].values():
+            ex = experiment_config.copy()
+            ex["dataset"] = dataset_name
+            experiments[run_id(ex, dataset_name)] = ex
+
+    config["experiments"] = experiments
+
+    return config
+
+def run_inference(run_id, experiment_config) -> datasets.Dataset:
+    """Run the inference for a given experiment configuration.
+    This function would typically call the actual inference logic.
+    """
+    dst_dir = f"{run_id}/inference"
+    # Try to load the existing results.
+    if os.path.exists(dst_dir):
+        prediction_dataset = datasets.load_from_disk(dst_dir)
+        if "answer_pred" in prediction_dataset.column_names:
+            print(f"> Skipping inference for {run_id} as results already exist.")
+            return prediction_dataset
+        else:
+            raise Exception(f"{run_id}: This shouldn't happen: missing 'answer_pred' column but dataset exists.")
+
+    print(f"> Running inference for {run_id}.")
+    print(f"> Loading dataset {experiment_config['dataset']}.")
+    dataset = load_dataset(experiment_config["dataset"])
+
+    # Set up the pipeline.
+    pipeline_cls = PIPELINE_MAP.get(experiment_config["pipeline"])
+    if pipeline_cls is None:
+        raise ValueError(f"Pipeline {experiment_config['pipeline']} is not supported.")
+    if experiment_config["pipeline"] == "QAEvaluator":
+        pipeline = pipeline_cls(
+                    model_name=experiment_config["model_name"],
+                    openai_client=openai_client,
+                    temperature=experiment_config["temperature"],
+                    n_trials=N_TRIALS,
+        )
+    else:
+        pipeline = pipeline_cls(
+                    highlighter_model_name=experiment_config["highlighter_model_name"],
+                    summarizer_model_name=experiment_config["summarizer_model_name"],
+                    openai_client=openai_client,
+                    temperature=experiment_config["temperature"],
+                    n_trials=N_TRIALS,
+        )
+
+    # Run.
+    now = time.time()
+    prediction_dataset = dataset.map(
+        pipeline,
+        desc=f"> Generating predictions",
+        num_proc=N_PROC,
+        load_from_cache_file=False, # We handle cache separately.
+        )
+    elapsed_time = time.time() - now
+
+    with open(f"{run_id}/run.txt", "w") as f:
+        f.write(f"Elapsed time for predictions: {elapsed_time:.2f} seconds. Average: {elapsed_time / len(dataset):.2f} seconds per example.")
+    
+    # Store.
+    print(f"> Storing predictions to {dst_dir}.")
+    prediction_dataset.save_to_disk(dst_dir)
+
+    return prediction_dataset
+
+
+def run_judgement(run_id, prediction_dataset) -> datasets.Dataset:
+    """Run the judgement for a given experiment configuration.
+    This function would typically call the actual judgement logic.
+    """
+    dst_dir = f"{run_id}/judgement"
+    # Try to load the existing results.
+    if os.path.exists(dst_dir):
+        print(f"> Skipping judgement for {run_id} as results already exist.")
+        return datasets.load_from_disk(dst_dir)
+
+    # Run.
+    judge = LLMJudgeStructured(openai_client=openai_client)
+    judged_dataset = prediction_dataset.map(
+        judge,
+        desc=f"> Judging predictions",
+        num_proc=N_PROC,
+        load_from_cache_file=False,
+    )
+    # Store.
+    print(f"> Storing judged predictions to {dst_dir}.")
+    judged_dataset.save_to_disk(dst_dir)
+
+    return judged_dataset
+
+def load_all_results(results_dir="results/") -> dict[str, Any]:
+    """Load all results from the results directory.
+    """
+    results = {}
+    for dataset_name in os.listdir(results_dir):
+        dataset_path = os.path.join(results_dir, dataset_name)
+        if os.path.isdir(dataset_path):
+            for pipeline_name in os.listdir(dataset_path):
+                run_id = os.path.join(dataset_path, pipeline_name)
+                if os.path.isdir(run_id):
+                    if not os.path.exists(os.path.join(run_id, "experiment.yaml")):
+                        continue
+                    config = yaml.safe_load(open(os.path.join(run_id, "experiment.yaml"), "r"))
+                    judgement_dir = os.path.join(run_id, "judgement")
+                    if os.path.exists(judgement_dir):
+                        # Load the judgement results.
+                        judged = datasets.load_from_disk(judgement_dir)
+                        results[run_id] = {
+                            "config": config,
+                            "judged_dataset": judged,
+                        }
+    return results
+
+
+if __name__ == "__main__":
+    config = load_config("experiments.yaml")
+
+    for run_id, experiment_config in config["experiments"].items():
+        os.makedirs(run_id, exist_ok=True)
+        print(f"[*] Running experiment: {run_id}")
+        # Store the configuration in the run directory.
+        if not os.path.exists(f"{run_id}/experiment.yaml"):
+            with open(f"{run_id}/experiment.yaml", "w") as f:
+                yaml.dump(experiment_config, f)
+        else:
+            # Check if the experiment config has changed.
+            with open(f"{run_id}/experiment.yaml", "r") as f:
+                existing_config = yaml.safe_load(f)
+            if existing_config != experiment_config:
+                raise ValueError(
+                    f"Experiment configuration for {run_id} has changed. "
+                    "Please remove the existing directory to rerun the experiment."
+                )
+
+        prediction_dataset = run_inference(run_id, experiment_config)
+        run_judgement(run_id, prediction_dataset)
