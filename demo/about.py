7eb10edfffdcb7027b7fb0ae4bc75dceddc7ac2f
Giovanni Cherubin <gcherubin@microsoft.com>
2025-07-04 12:54:32 +0100

    disable download paper

--- demo/about.py
+++ demo/about.py
@@ -8,19 +8,13 @@ def load_content(fname, mode="r"):
     with open(os.path.join(CONTENTS_DIR, fname), mode) as f:
         return f.read()
 
-with st.sidebar:
-    pdf = load_content("highlight_summarize.pdf", mode="rb")
+# pdf = load_content("highlight_summarize.pdf", mode="rb")
 
-    st.download_button(label="Get the paper",
-                        data=pdf,
-                        file_name="highlight_summarize.pdf",
-                        mime='application/octet-stream'
-    )
-    # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    # "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    # "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    # "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
+# st.sidebar.download_button(label="Get the paper",
+#                         data=pdf,
+#                         file_name="highlight_summarize.pdf",
+#                         mime='application/octet-stream'
+# )
 
 
 def img_to_bytes(img_path):
