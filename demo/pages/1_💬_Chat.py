9264bf9ebfeda94965cd3c264336ebe145624803
Giovanni Cherubin <gcherubin@microsoft.com>
2025-07-08 15:30:51 +0100

    fix: accordion

--- demo/pages/1_💬_Chat.py
+++ demo/pages/1_💬_Chat.py
@@ -1,9 +1,10 @@
 import sys
 import streamlit as st
+from pydantic import BaseModel
 
 from utils import sidebar, load_content, log
 sys.path.append("..")
-from src.hs import HSStructuredHighlighter
+from src.hs import HSStructuredHighlighter, HSBaselinePrediction
 
 #################################
 # H&S and data.
@@ -27,41 +28,65 @@ detective = load_content("detective.png", mode="rb")
 st.title("💬 Highlight&Summarize")
 st.caption("🚀 A demo of H&S, which can answer questions about H&S!")
 sidebar()
+# show_internals = st.toggle("Show the internal process of H&S", value=False)
+show_internals = True
+
+class Msg(BaseModel):
+    role: str
+    content: str
+    full_response: HSBaselinePrediction | None = None
+
+    def display(self):
+        """Display the message in the chat."""
+        if self.full_response and show_internals:
+            self.display_intermediate()
+        st.chat_message(self.role).write(self.content)
+
+    def display_intermediate(self):
+        """Display the intermediate outputs of the H&S model."""
+        if not self.full_response:
+            raise ValueError("No full response to display.")
+        md = "Psst! Here's what is happening inside H&S:\n\n"
+        lines = [
+            f"**Highlighter Output:** {self.full_response.highlighter_extracted}.",
+            f"**What question the summarizer thinks was asked:** {self.full_response.summarizer_llm_guessed_question}.",
+            f"**Summarizer response:** {self.full_response.answer_pred}."
+        ]
+        for line in lines:
+            md += f":blue[{line}]\n\n"
+        with st.expander("See the intermediate outputs in H&S", expanded=False):
+            st.chat_message(name="detective", avatar=detective).write(md)
 
 
+# Initialize session state.
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Hi! I can answer questions about H&S. If there's something I don't know, I'll just say so. How can I help you?"}]
+    st.session_state["messages"] = [
+        Msg(role="assistant",
+            content="Hi! I can answer questions about H&S. If there's something I don't know, I'll just say so. How can I help you?")
+    ]
 
+# Display chat history.
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    msg.display()
 
 if prompt := st.chat_input():
-    st.chat_message("user").write(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    msg = Msg(role="user", content=prompt)
+    msg.display()
+    st.session_state.messages.append(msg)
+
     response = hs.call_model(
         context_str=doc,
         question_str=prompt
     )
 
-    # Display.
-    md = "Psst! Here's what is happening inside H&S:\n\n"
-    lines = [
-        f"**Highlighter Output:** {response.highlighter_extracted}.",
-        f"**What question the summarizer thinks was asked:** {response.summarizer_llm_guessed_question}.",
-        f"**Summarizer response:** {response.answer_pred}."
-    ]
-    for line in lines:
-        md += f":blue[{line}]\n\n"
-    with st.expander("See the intermediate outputs in H&S", expanded=False):
-        st.chat_message(name="detective", avatar=detective).write(md)
-    st.session_state.messages.append({"role": "detective", "content": md})
-
+    # Handle the response.
     if not response.answer_pred or response.answer_pred == "UNANSWERABLE":
-        st.chat_message("assistant").write("I don't know the answer to that question. I can only answer based on my knowledge about H&S.")
-        st.session_state.messages.append({"role": "assistant", "content": "I don't know the answer to that question."})
+        answer = "I don't know the answer to that question. I can only answer based on my knowledge about H&S."
     else:
-        st.chat_message("assistant").write(response.answer_pred)
-        st.session_state.messages.append({"role": "assistant", "content": response.answer_pred})
+        answer = response.answer_pred
+    msg = Msg(role="assistant", content=answer, full_response=response)
+    msg.display()
+    st.session_state.messages.append(msg)
 
     # Logging.
     log({
