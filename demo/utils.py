80f4aca78e368b84abba824ac8d0a67aa0f6d821
Giovanni Cherubin <gcherubin@microsoft.com>
2025-07-14 11:39:30 +0100

    fix: better pdf serving

--- demo/utils.py
+++ demo/utils.py
@@ -14,20 +14,9 @@ def log(data: dict):
         json.dump(data, f, ensure_ascii=False)
         f.write('\n')
 
-def load_content(fname, mode="r"):
-    """Load content from a file in the contents directory."""
-    with open(os.path.join(CONTENTS_DIR, fname), mode) as f:
-        return f.read()
-
-pdf = load_content("highlight_summarize.pdf", mode="rb")
-
 def sidebar():
     with st.sidebar:
         st.title("Highlight & Summarize")
         st.markdown("A RAG design pattern for secure, high-quality LLM-based question answering.")
-        st.download_button(label="📖 Get the paper",
-                                data=pdf,
-                                file_name="highlight_summarize.pdf",
-                                mime='application/octet-stream'
-        )
+        st.markdown("📖 [Read the paper](app/static/highlight-summarize.pdf)")
         st.markdown('<img src="https://pngimg.com/d/github_PNG40.png" width="30"> [Code](https://github.com/microsoft/highlight-summarize)', unsafe_allow_html=True)
